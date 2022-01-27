eb08ede46364f5d47017f6c153583aadccd1b408
Alexander Flanagan <alex.flanagan@gmail.com>
2022-01-27 10:36:52 -0500

    feat(folders): Add folder uploads to action.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const util = require('util')
 
 const core = require('@actions/core')
@@ -9,13 +10,33 @@ const FormData = require('form-data')
 const glob = util.promisify(require('glob'))
 const exec = util.promisify(require('child_process').exec)
 
+// If a path is a directory, recursively add all files within it to the form-data object.
+/*async function recurseDirectory(form, prefix, pathname) {
+    await glob(path.join(pathname, '**')).then(async entries => {
+        for (let i = 0; i < entries.length; i++) {
+            const lstat = fs.lstatSync(entries[i])
+            if (lstat.isDirectory()) {
+                // The first path the glob enumerates is the current path, skip.
+                if (path.normalize(pathname) != path.normalize(entries[i])) {
+                    recurseDirectory(form, prefix + i + '_', entries[i])
+                }
+            } else if (lstat.isFile()) {
+                form.append(prefix + i, fs.createReadStream(entries[i]))
+            } else {
+                throw('Unrecognized lstat: ', JSON.stringify(lstat))
+            }
+        }
+    })
+}*/
+
 async function start() {
     try {
         const secret = core.getInput('secret', { required: true })
         const globspec = core.getInput('glob', { required: true })
-        // Restrict the "as" parameter to either "dag" or "file".
-        const as = (core.getInput('as', { required: false }).match("^dag$|^file$") || ['dag'])[0]
         const published = core.getBooleanInput('published', { required: false }) || false
+
+        // Restrict the "as" parameter to either "dag", "file", or "dir", defaulting to "dag".
+        const as = (core.getInput('as', { required: false }).match("^dag$|^file$|^dir$") || ['dag'])[0]
 
         const roots = await glob(globspec).then(async files => {
             const roots = []
@@ -28,15 +49,35 @@ async function start() {
 
                 const form = new FormData()
 
+                const inodeStat = fs.lstatSync(files[i])
+
                 if ("dag" == as) {
+                    if (!inodeStat.isFile()) {
+                        throw ('"dag" parameter requires a file, got: ', JSON.stringify(inodeStat))
+                    }
                     // TODO: Make this call via SDK instead of CLI.
                     // TODO: Check failure to pack.
+                    // TODO: When the `json` command lands, s/courtyard/json/.
                     const { stdin, stdout } = await exec(`node ${__dirname}/vendor/cli.js courtyard convert ${files[i]}`)
                     //options.body = fs.createReadStream('./output.car')
                     form.append('data', fs.createReadStream('./output.car'))
-                } else { // "file" == as
+                } else if ("file" == as) {
+                    if (!inodeStat.isFile()) {
+                        throw ('"file" parameter requires a file, got: ', JSON.stringify(inodeStat))
+                    }
                     //options.body = fs.createReadStream(files[i])
                     form.append('data', fs.createReadStream(files[i]))
+                } else if ("dir" == as) {
+                    if (!inodeStat.isDirectory()) {
+                        throw ('"dir" parameter requires a directory, got: ', JSON.stringify(inodeStat))
+                    }
+                    // TODO: Use a secure upload key and do this client-side?
+                    // TODO: Pack the directory by adding all the files to the body.
+                    // TODO: What if there are duplicate names? Is there a path differentiator?
+                    const { stdin, stdout } = await exec(`node ${__dirname}/node_modules/ipfs-car/dist/cjs/cli/cli.js --pack ${files[i]} --output output.car`)
+                    form.append('data', fs.createReadStream('./output.car'))
+                } else {
+                    throw ('Invalid "as" parameter: ', as)
                 }
 
                 const options = {
