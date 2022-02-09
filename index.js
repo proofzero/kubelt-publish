98aaae131d8c206c17f4298ff2df0eb4e8456c22
alfl <alex.flanagan@gmail.com>
2022-02-09 08:42:23 -0500

    wip

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
 const fs = require('fs')
 const path = require('path')
-const util = require('util')
 
 const core = require('@actions/core')
 const fetch = require('node-fetch')
-const sha256 = require('@stablelib/sha256')
+const crypto = require('libp2p-crypto')
 const FormData = require('form-data')
+const { base36 } = require('multiformats/bases/base36')
+const PeerId = require('peer-id')
+const { CID } = require('multiformats/cid')
 
+const util = require('util')
 const glob = util.promisify(require('glob'))
 const exec = util.promisify(require('child_process').exec)
 
-// If a path is a directory, recursively add all files within it to the form-data object.
-/*async function recurseDirectory(form, prefix, pathname) {
-    await glob(path.join(pathname, '**')).then(async entries => {
-        for (let i = 0; i < entries.length; i++) {
-            const lstat = fs.lstatSync(entries[i])
-            if (lstat.isDirectory()) {
-                // The first path the glob enumerates is the current path, skip.
-                if (path.normalize(pathname) != path.normalize(entries[i])) {
-                    recurseDirectory(form, prefix + i + '_', entries[i])
-                }
-            } else if (lstat.isFile()) {
-                form.append(prefix + i, fs.createReadStream(entries[i]))
-            } else {
-                throw('Unrecognized lstat: ', JSON.stringify(lstat))
-            }
-        }
-    })
-}*/
+async function getContentName(publishingKey, name) {
+    const peer = await PeerId.createFromPrivKey(publishingKey.bytes)
+    const peerId = peer.toString()
+    const cid = CID.parse(peerId)
+    return cid.toString(base36)
+}
+
+async function getPublishingKey(secret, name) {
+    // Get the key object out of the protobuf.
+    const secretKey = crypto.keys.UnmarshalPrivateKey(secret)
+
+    // Sign the human name to seed the generation of a publishing key for it.
+    const signature = await secretKey.sign(Buffer.from(name))
+
+    // ed25519 seeds must be 32 bytes.
+    const seedStartIndex = 0
+    const seedLength = 32
+    const seed = signature.slice(seedStartIndex, seedLength)
+
+    // Return a 2048 bit ed25519 keypair with the above seed.
+    const algo = 'ed25519'
+    const bitwidth = 2048
+    return crypto.keys.generateKeyPairFromSeed(algo, seed, bitwidth)
+}
+
+function getHumanName(namespec, filepath) {
+    let name = ''
+    // TODO: Currently namespec is only expected to be "path". Add other specs.
+    // TODO: Should probably done inline with the "as" parameter handling.
+    switch (namespec) {
+        case "path":
+            // Get the filename at the end of the passed path.
+            const namebase = path.basename(filepath)
+            // Strip off the extension and use this as the content name.
+            name = namebase.substring(0, namebase.lastIndexOf('.'))
+        break
+        default:
+            throw(`Unexpected namespec ${namespec}. Should be 'path'.`)
+    }
+    return name
+}
 
 async function start() {
     try {
         const secret = core.getInput('secret', { required: true })
         const globspec = core.getInput('glob', { required: true })
+        const namespec = core.getInput('name', { required: false }) || 'path'
         const published = core.getBooleanInput('published', { required: false }) || false
 
         // Restrict the "as" parameter to either "dag", "file", "dir", or "wrap", defaulting to "dag".
@@ -91,18 +118,26 @@ async function start() {
                 }
 
                 options.body = form
+
+                const humanName = getHumanName(namespec, files[i])
+                const publishingKey = await getPublishingKey(secret, humanName)
+                const contentName = await getContentNamme(publishingKey, humanName)
+
+                // TODO: Send key to server?
+                // Pack the key in a protobuf for transmission.
+                const encodedKey = crypto.keys.marshalPrivateKey(publishingKey)
  
                 // TODO: Calculate the name so we can build it into the URL.
-                const name = Buffer.from(sha256.hash(secret + files[i])).toString('hex')
+                //const name = Buffer.from(sha256.hash(secret + files[i])).toString('hex')
 
                 // TODO: Figure out how to make work with `act` for local dev.
                 //const url = new URL(name, 'http://127.0.0.1:8787/v0/api/content/kbt/')
-                const url = new URL(name, 'https://api.pndo.xyz/v0/api/content/kbt/')
+                const url = new URL(contentName, 'https://api.pndo.xyz/v0/api/content/kbt/')
                 //console.log(url)
 
                 // Push the CID into the roots list.
                 responseBody = await fetch(url, options).then(response => response.json())
-                responseBody.metadata.box.name = `/kbt/${name}`
+                responseBody.metadata.box.name = `/kbt/${contentName}`
                 //console.log(responseBody)
                 roots.push(responseBody)
             }
